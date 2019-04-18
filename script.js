105dad31a47f0c98c04fca869b8a7b842ed36690
Camilo Dominguez <camiloa17@gmail.com>
2019-04-18 13:11:17 -0300

    remove popup and change red class for green class

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function inicializar() {
     //Acá va el código de inicialización
+    document.querySelector(".blink").style.display = "none";
+    document.querySelector("body").style.backgroundColor = "#FAFBFC";
+    
+    for(let redClass of document.querySelectorAll(".rojo")){
+        redClass.classList.add("verde");
+        redClass.classList.remove("rojo");
+    }
 
 }
